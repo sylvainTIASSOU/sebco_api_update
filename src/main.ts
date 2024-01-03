f74422fa8ed976ccadbe208dc664cbba10db811a
sylvainTIASSOU <tiassousylvain6120@gmail.com>
2024-01-03 07:41:09 +0000

    commit4

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // the next two lines did the trick
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3001);
